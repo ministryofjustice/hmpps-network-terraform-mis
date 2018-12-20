6860d0ca1daf05009c1c2a2b08295a51097704b4
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-12-20 10:03:27 +0000

    Added bastion and eng outputs

--- vpc/main.tf
+++ vpc/main.tf
@@ -12,3 +12,31 @@ provider "aws" {
 ### Getting the current running account id
 #-------------------------------------------------------------
 data "aws_caller_identity" "current" {}
+
+#-------------------------------------------------------------
+### Getting the engineering platform vpc
+#-------------------------------------------------------------
+data "terraform_remote_state" "eng_remote_vpc" {
+  backend = "s3"
+
+  config {
+    bucket   = "${var.eng_remote_state_bucket_name}"
+    key      = "vpc/terraform.tfstate"
+    region   = "${var.region}"
+    role_arn = "${var.eng_role_arn}"
+  }
+}
+
+#-------------------------------------------------------------
+### Getting the bastion vpc
+#-------------------------------------------------------------
+data "terraform_remote_state" "bastion_remote_vpc" {
+  backend = "s3"
+
+  config {
+    bucket   = "${var.bastion_remote_state_bucket_name}"
+    key      = "bastion-vpc/terraform.tfstate"
+    region   = "${var.region}"
+    role_arn = "${var.bastion_role_arn}"
+  }
+}
